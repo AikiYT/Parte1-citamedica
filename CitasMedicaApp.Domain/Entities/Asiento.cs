6c23a6e2f4128416eab1ecfff967be5a1be29b03
Iverson Inoel Rosario De Jesus <iversoninoel@gmail.com>
2024-09-27 19:11:37 -0400

    Add project files.

---
+++ CitasMedicaApp.Domain/Entities/Asiento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace CitasMedicaApp.Domain.Entities
+{
+    public sealed class Asiento
+    {
+       public int IdAsiento { get; set; }  
+
+       public int IdBus { get; set; }
+
+        public int NumeroPiso { get; set; } 
+
+        public int NumeroAsiento { get; set; }
+
+        public DateTime FechaCreacion { get; set; } 
+
+        public DateTime? FechaModificacion { get; set; }    
+
+        public DateTime? UsuarioModificacion { get; set; }  
+
+        public bool Estatus {  get; set; }  
+
+
+
+
+
+    }
+}
